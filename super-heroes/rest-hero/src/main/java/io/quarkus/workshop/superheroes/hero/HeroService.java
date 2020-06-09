0f19bdc0f109b8d5b7e1e5f1e7fefa84262e194a
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-06-08 23:17:11 -0300

    Adicionando valor de configuração de injeção

--- super-heroes/rest-hero/src/main/java/io/quarkus/workshop/superheroes/hero/HeroService.java
+++ super-heroes/rest-hero/src/main/java/io/quarkus/workshop/superheroes/hero/HeroService.java
@@ -1,5 +1,7 @@
 package io.quarkus.workshop.superheroes.hero;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
@@ -11,6 +13,9 @@ import static javax.transaction.Transactional.TxType.SUPPORTS;
 @ApplicationScoped
 @Transactional(REQUIRED)
 public class HeroService {
+
+    @ConfigProperty(name = "level.multiplier", defaultValue = "1")
+    int levelMultiplier;
 
 
     @Transactional(SUPPORTS)
@@ -33,6 +38,7 @@ public class HeroService {
     }
 
     public Hero persistHero(@Valid Hero hero) {
+        hero.level = hero.level * levelMultiplier;
         Hero.persist(hero);
         return hero;
     }
