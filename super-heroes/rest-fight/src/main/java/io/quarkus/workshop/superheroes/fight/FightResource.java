e5c91cf58d879e3beaffc7ab7556b7d643ea53eb
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-06-13 19:26:51 -0300

    Resource Fight

--- super-heroes/rest-fight/src/main/java/io/quarkus/workshop/superheroes/fight/FightResource.java
+++ super-heroes/rest-fight/src/main/java/io/quarkus/workshop/superheroes/fight/FightResource.java
@@ -1,12 +1,65 @@
 package io.quarkus.workshop.superheroes.fight;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.jboss.logging.Logger;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Path("/api/fights")
+@Produces(APPLICATION_JSON)
 public class FightResource {
+
+    private static final Logger LOGGER = Logger.getLogger(FightResource.class);
+
+    @Inject
+    FightService service;
+
+    @GET
+    @Path("/randomfighters")
+    public Response getRandomFighters() throws InterruptedException {
+        Fighters fighters = service.findRandomFighters();
+        LOGGER.debug("Get random fighters " + fighters);
+        return Response.ok(fighters).build();
+    }
+
+    @GET
+    public Response getAllFights() {
+        List<Fight> fights = service.findAllFights();
+        LOGGER.debug("Total number of fights " + fights);
+        return Response.ok(fights).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getFight(@Parameter(description = "Fight identifier", required = true) @PathParam("id") Long id) {
+        Fight fight = service.findFightById(id);
+        if (fight != null) {
+            LOGGER.debug("Found fight " + fight);
+            return Response.ok(fight).build();
+        } else {
+            LOGGER.debug("No fight found with id " + id);
+            return Response.noContent().build();
+        }
+    }
+
+    @POST
+    public Fight fight(@RequestBody(description = "The two fighters fighting", required = true,
+        content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Fighters.class)))
+                           @Valid Fighters fighters, @Context UriInfo uriInfo) {
+        return service.persistFight(fighters);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
